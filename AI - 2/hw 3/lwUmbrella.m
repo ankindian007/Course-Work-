80b8d842cbefb0c885e0d6b022c0666fc2089134
ankindian007 <ankindian007@gmail.com>
2013-04-06 01:02:16 -0500

    4/6/2013 Night course AI-2 push:2

--- AI - 2/hw 3/lwUmbrella.m
+++ AI - 2/hw 3/lwUmbrella.m
@@ -3,7 +3,7 @@ function [P,variance] = lwUmbrella(numSamples, numSteps, evidence)
 P_r = [0.3 0.7]; P_u = [0.1 0.9];
 U = evidence+1; % Evidences vector (1 X numSteps)
 R = zeros(1,numSteps); % Hidden variable vector (1 X numSteps)
-
+P=[];
 for n = 1:numSamples
     R(1,1) = (rand >= 0.5)+1; % Equal probability of first hidden variable.
     w = 1; % initalize weight of evidence
@@ -12,13 +12,12 @@ for n = 1:numSamples
     for i = 1:numSteps
         w = w*P_u(U(i));
     end
-    w
+    
     for i = 2:numSteps
-        i
-        R(1,i-1)
-        x = (rand >= 0.5)+1;
-        
-        S = S * P_r(R(1,i-1));
+        %i
+        %R(1,i-1)
+        x = (rand >= 0.5)+1;        
+        S = S * P_r(x);
     end
     
     P = [P w*S]; % Weighted sampling probability.
